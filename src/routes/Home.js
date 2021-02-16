78fc5c44aa2d65fbd584d60d3c9a0dde5e08c346
kongs <yoursitup90@gmail.com>
2021-02-16 10:48:26 +0900

    추가 구현사항 21.02.16 1.이미지 미리보기 2.이미지 트윗구현 3.이미지+텍스트 트윗구현(에러수정)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,13 @@
 import Nweet from "components/Nweet";
-import {dbService} from "fbase";
+import {dbService,storageService} from "fbase";
+import {v4 as uuidv4} from "uuid";
 import React, {useEffect, useState} from "react";
 
 const Home = ({userObj}) => {
     console.log(userObj);
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment,setAttachment] = useState("");
     const getNeweets = async () => {
         const dbNweets = await dbService
             .collection("nweets")
@@ -23,6 +25,7 @@ const Home = ({userObj}) => {
     useEffect(() => {
         dbService
             .collection("nweets")
+            .orderBy("createdAt", "desc")
             .onSnapshot((Snapshot) => {
                 const nweetArray = Snapshot
                     .docs
@@ -36,10 +39,24 @@ const Home = ({userObj}) => {
 
     const onSubmit = async (event) => {
         event.preventDefault();
+        let attachmentUrl =""; 
+        if (attachment != ""){
+            const attachmentRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+            const response = await attachmentRef.putString(attachment,"data_url");
+            attachmentUrl = await response.ref.getDownloadURL();
+        }
+            const nweetObj = {
+                text: nweet,
+                createdAt: Date.now(),
+                creatorId: userObj.uid,
+                attachmentUrl,
+        }
         await dbService
             .collection("nweets")
-            .add({text: nweet, createdAt: Date.now(), creatorId: userObj.uid});
+            .add(nweetObj);
         setNweet("");
+        setAttachment("");
+
     };
     const onChange = (event) => {
         const {target: {
@@ -47,23 +64,44 @@ const Home = ({userObj}) => {
             }} = event;
         setNweet(value);
     }
-    console.log(nweets);
-    return (<div>
-        <form onSubmit={onSubmit}>
-            <input
-                value={nweet}
-                onChange={onChange}
-                type="text"
-                placeholder="What's on your mind"
-                maxLength={120}/>
-            <input type="submit" value="click"/>
-        </form>
+    const onFileChange = (event) =>{
+        const {target:{files},
+        } = event;
+        const theFile = files[0];
+        const reader =new FileReader();
+        reader.onloadend = (finishedEvent) =>{
+            console.log(finishedEvent);
+            const{
+            currentTarget:{
+                result
+            },
+            } = finishedEvent;
+            setAttachment(result);
+        }
+        reader.readAsDataURL(theFile);
+        //console.log(event.target.files)
+        //console.log(theFile)
+    };
+    const onClearAttachment = () => setAttachment("");
+    return (
         <div>
-            {nweets.map((nweet) => (
-                <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid} />
+            <form onSubmit={onSubmit}>
+                <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind" maxLength={120}/>
+                <input type="file" accept="image/*" onChange={onFileChange}/>
+                <input type="submit" value="click"/>
+                {attachment && (
+                    <div>
+                        <img src ={attachment} width="50px" height="50px" />
+                        <button onClick={onClearAttachment}>clear</button>
+                    </div>
+                )}
+            </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid} />  
                 ))} 
-                </div>
-                </div >
-                );
-            };
+            </div>
+        </div >
+        );
+};
 export default Home;
